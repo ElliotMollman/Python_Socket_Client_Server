a5dfd034238d047bc4a614b51cd6d54aab6464c1
Elliot Mollman <129421289+ElliotMollman@users.noreply.github.com>
2025-10-05 16:29:26 -0700

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,24 @@
+import socket
+def send_data(dest_ip, dest_port, message):
+    print(f"sending message to {dest_ip}:{dest_port}...\n")
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCP_sock:
+            # socket.AF_INET tells python the address family we are using, which is ipv4. We can use socket.AF_INET6 for ipv6 addresses.
+            # SOCK_STREAM is the socket type for TCP, and the timeout for packets is specified as 10 seconds.
+            TCP_sock.connect((dest_ip, dest_port))
+            # .connect() will keep connection alive until closed.
+            TCP_sock.settimeout(5)
+            TCP_sock.sendall(message.encode('utf-8'))
+            print("waiting for reply...\n")
+            data = TCP_sock.recv(1024)
+            print(data.decode('utf-8'))
+            # This method repeatedly calls socket.send() internally until all of the bytes data has been sent, or an error occurs.
+            # It also handles the partial sends automatically.
+    except:
+        print("failed to connect")
+
+
+dest_port = 5132
+dest_ip = '127.0.0.1'
+message = input("Enter message to send: \n")
+send_data(dest_ip, dest_port, message)
---
+++ Server.py
@@ -0,0 +1,34 @@
+import socket
+def receive_data(host, server_port, reply):
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCP_sock:
+		# socket.AF_INET tells python the address family we are using, which is ipv4. We can use socket.AF_INET6 for ipv6 addresses.
+		# SOCK_STREAM is the socket type for TCP, and the timeout for packets is specified
+		TCP_sock.bind((host, server_port))
+		# Will bind to itself to the client host and port we want to listen to.
+		TCP_sock.listen(5)
+		print(f"listening for connections...on port {server_port}\n")
+		# Open to connections, with a maximum of 5 at once. No other processes will run while waiting for a connection
+		conn, client_addr = TCP_sock.accept()
+		with conn:
+			conn.settimeout(15)
+			#This method controls how long the socket will wait for an operation \(like connect(), recv(), or send()) to complete.
+			print(f"Connected to: {client_addr}")
+			while True:
+				try:
+					data = conn.recv(2048)
+					if data:
+						print("Data from Client: " + data.decode('utf-8'))
+						# Receiving a maximum of 1 kilobyte of data from client. The recv() function is blocking by default like listen().
+					elif not data:
+						# If the server doesn't receive any data, then it terminates the connection.
+						break
+					conn.sendall(reply.encode('utf-8'))	
+				except socket.timeout:
+					print("Receive time out.")
+host = ""
+# Empty string will accet any incoming ip connection
+server_port = 5132
+reply = 'Server: connection finished. Disconnecting........'
+while True:
+	receive_data(host, server_port, reply)
+	# Continuous loop for listening to connections.
